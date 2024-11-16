811ab599933bf88b439fcc3ec40d8be72821509b
Strelok656 <almazgallyamov10@mail.ru>
2024-11-16 16:28:20 +0500

    Finished Wiki Pages 	new file:   Python/quadralist.py 	modified:   css/WikiPages.css 	new file:   img/Crimtane_Bar.png 	new file:   img/Crimtane_Ore.png 	new file:   img/Fallen_Star.png 	new file:   img/Hellstone_Bar.png 	modified:   index.html 	modified:   wiki-pages/Absorber.html 	modified:   wiki-pages/AshSlicer.html 	modified:   wiki-pages/BloomingCrimtane.html 	modified:   wiki-pages/CoolStar.html

---
+++ Python/quadralist.py
@@ -0,0 +1,32 @@
+# n = int(input())
+# i = 1
+
+# while i ** 2 <= n:
+#    print(i ** 2, end=" ")
+#    i = i + 1
+
+# n = int(input())
+# i = 2
+
+# while n % i != 0:
+#    i+=1
+# print(i)
+
+
+# N = int(input())
+# i = 2
+# x = 1
+# if N > 0:
+#    while i * 2 <= N:
+#       i = i * 2
+#       x += 1
+# print(x, i)
+
+x = int(input())
+y = int(input())
+z = 1
+
+while x < y:
+   x = x * 1.1
+   z += 1
+print(z)
