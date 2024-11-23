0bf388fd274e6c8486b6477ff3a8e95e6d656995
Strelok656 <almazgallyamov10@mail.ru>
2024-11-23 16:28:26 +0500

    new file:   Python/string1.py 	renamed:    index.html -> main.html

---
+++ Python/string1.py
@@ -0,0 +1,20 @@
+# s = input()
+
+# for i in range(len(s)):
+#     print(s[-i - 1])
+
+# s = input()
+
+# for i in range(len(s)):
+#     print(i*' ',s[i:-i])
+
+# s = input()
+
+# if s.find('борщ') > 0:
+#     print('Да')
+# else:
+#     print('Нет')
+
+s = input()
+
+print(s.count(' ') + 1)
