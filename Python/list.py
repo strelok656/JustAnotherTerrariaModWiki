6571756218d5b3bb2acefcb36d3ab513ba7e6a0d
Strelok656 <almazgallyamov10@mail.ru>
2024-12-07 16:35:29 +0500

    ам ам ам амамамам 	new file:   Python/list.py 	new file:   Python/sets.py 	new file:   account.html 	new file:   css/Account.html 	modified:   css/Common.css 	modified:   css/WikiPages.css 	new file:   img/SiteIcon.png 	renamed:    main.html -> index.html 	modified:   wiki-pages/Absorber.html 	modified:   wiki-pages/AshSlicer.html 	modified:   wiki-pages/BloomingCrimtane.html 	modified:   wiki-pages/CoolStar.html

---
+++ Python/list.py
@@ -0,0 +1,74 @@
+# a = []
+# a.append(4)
+# Primes = [2, 3, 5, 7, 11, 13]
+
+# a = [1, 2, 3]
+# b = [4, 5]
+# c = a + b
+# d = b * 3
+
+# print(c, d)
+
+# Rainbow = ["Red", "Orange", "Yellow", "Green"]
+# Rainbow[0] = 'Красный'
+
+# for i in range(len(Rainbow)):
+#     print(Rainbow[i])
+
+# a = [1, 2, 3, 4, 5]
+# for elem in a:
+#     print(elem, end=' ')
+
+# a = input()
+# b = a.split()
+
+# for i in range(len(b)):
+#     c = int(b[i])
+#     if(c % 2 == 0):
+#         print(c, end=' ')
+
+# a = input().split()
+
+# for i in range(len(a)):
+#     if(i % 2 == 0):
+#         print(a[i], end=' ')
+
+# a = input().split()
+# b = int(input())
+# d = 1
+
+# for i in range(len(a)):
+#     c = int(a[i])
+#     if c >= b:
+#         d+=1
+# print(d)
+
+# a = input().split()
+# b = int(input())
+# x = a[b]
+# a.append(x)
+# a.pop()
+# print(a)
+
+# list = input().split()
+# k = int(input())
+# right_list = []
+# left_list = []
+
+# for i in range(len(list)):
+#     if i <= k:
+#         left_list += list[i]
+#     else:
+#         right_list += list[i]
+
+# left_list.pop()
+# print(left_list + right_list)
+
+# A = input().split()
+# k = int(input())
+
+# for i in range(k + 1, len(A)):
+#     A[i - 1] = A[i]
+# A.pop()
+
+# print(A)
---
+++ Python/sets.py
@@ -0,0 +1,45 @@
+# A = {1, 2, 3}
+# A = set('qwertyy')
+# print(A)
+
+# primes = {2, 3, 5, 7, 11}
+
+# for num in primes:
+#     print(num)
+
+# A = {1, 2, 3}
+# print(1 in A, 4 not in A)
+# A.add(4)
+# A.discard(2)
+# B = {3, 4, 5}
+
+# A | B
+# A.union(B)
+
+# A & B
+# A.intersection(B)
+
+# B - A
+# B.difference(A)
+
+# A ^ B
+# A.symmetric_difference(B)
+
+# print(len(set(input().split())))
+# print(len(set(input().split())) & (set(input().split())))
+
+# A = input().split()
+# b = set()
+
+# for i in A:
+#     if i not in b:
+#         b.add(i)
+#         print("NO")
+#     else:
+#         print("YES")
+
+# a = set(input().split())
+# b = set(input().split())
+
+# c = a.intersection(b)
+# print(c)
