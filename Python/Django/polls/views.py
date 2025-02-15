85a12d004055e0963dfd6d82b08d201da9958fae
Strelok656 <almazgallyamov10@mail.ru>
2025-02-15 16:28:53 +0500

    modified:   Python/Django/db.sqlite3 	modified:   Python/Django/polls/__pycache__/admin.cpython-313.pyc 	modified:   Python/Django/polls/__pycache__/urls.cpython-313.pyc 	modified:   Python/Django/polls/__pycache__/views.cpython-313.pyc 	modified:   Python/Django/polls/admin.py 	modified:   Python/Django/polls/templates/detail.html 	new file:   Python/Django/polls/templates/results.html 	modified:   Python/Django/polls/views.py

--- Python/Django/polls/views.py
+++ Python/Django/polls/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse 
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
-
+from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,7 +16,19 @@ def detail(request, question_id):
     return render(request, 'detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse(f"Ты смотришь на результаты вопроса {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse(f"Ты голосуешь за вопрос {question_id}.")
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'detail.html', {
+            'question': question,
+            'error_message': "Ничего не выбрано!",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
